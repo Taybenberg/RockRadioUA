648a0c8a33e6b45f85a2f7c490091d075c6d8c57
Вдовенко Віталій <taybenberg@gmail.com>
2020-06-03 00:23:00 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Input;
+using Avalonia.Media.Imaging;
+using Avalonia.Threading;
+
+namespace RockRadioUA
+{
+    public class MainWindow : Window
+    {
+        Radio radio;
+
+        bool play = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+
+            try
+            {
+                radio = new Radio("https://rockradioua.online:8433/rockradio256");
+
+                radio.OnStatusUpdate += (s) =>
+                {
+                    Dispatcher.UIThread.InvokeAsync(() => this.FindControl<TextBlock>("TrackLabel").Text = s);
+                };
+
+                radio.OnTitleAndArtistUpdate += (s) =>
+                {
+                    Dispatcher.UIThread.InvokeAsync(() =>
+                    {
+                        var strs = s.Split(" - ");
+
+                        this.FindControl<TextBlock>("TrackLabel").Text = strs[1];
+                        this.FindControl<TextBlock>("ArtistLabel").Text = strs[0];
+
+                        var img = new WebClient().DownloadData("https://rockradioua.online/rockradioua_artwork.png");
+                        this.FindControl<Image>("Cover").Source = new Bitmap(new MemoryStream(img));
+                    });
+                };
+
+                radio.Start();
+            }
+            catch (Exception e)
+            {
+                this.FindControl<TextBlock>("TrackLabel").Text = e.Message;
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+            this.Icon = new WindowIcon(new MemoryStream(AppResources.Logo));
+            this.FindControl<Image>("Cover").Source = new Bitmap(new MemoryStream(AppResources.Logo));
+            this.FindControl<Image>("TitleIcon").Source = new Bitmap(new MemoryStream(AppResources.Logo));
+            this.FindControl<Image>("ExitBtn").Source = new Bitmap(new MemoryStream(AppResources.Exit));
+            this.FindControl<Image>("MinimizeBtn").Source = new Bitmap(new MemoryStream(AppResources.Minimize));
+            this.FindControl<Image>("PlaybackControl").Source = new Bitmap(new MemoryStream(AppResources.Stop));
+        }
+        
+        private void PlaybackControl_MouseLeftButtonUp(object sender, PointerReleasedEventArgs e)
+        {
+            play = !play;
+            
+            if (play)
+            {
+                radio.Start();
+
+                ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.StopActive));
+            }
+            else
+            {
+                radio.Stop();
+
+                ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.PlayActive));
+
+                this.FindControl<TextBlock>("TrackLabel").Text = "Відтворення призупинено";
+                this.FindControl<TextBlock>("ArtistLabel").Text = string.Empty;
+
+                this.FindControl<Image>("Cover").Source = new Bitmap(new MemoryStream(AppResources.Logo));
+            }
+        }
+        
+        private void PlaybackControl_MouseEnter(object sender, PointerEventArgs e)
+        {
+            if (play)
+            {
+                ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.StopActive));
+            }
+            else
+            {
+                ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.PlayActive));
+            }
+        }
+
+        private void PlaybackControl_MouseLeave(object sender, PointerEventArgs e)
+        {
+            if (play)
+            {
+                ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.Stop));
+            }
+            else
+            {
+                ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.Play));
+            }
+        }
+
+        private void ExitBtn_MouseEnter(object sender, PointerEventArgs e)
+        {
+            ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.ExitActive));
+        }
+
+        private void ExitBtn_MouseLeave(object sender, PointerEventArgs e)
+        {
+            ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.Exit));
+        }
+
+        private void ExitBtn_MouseLeftButtonUp(object sender, PointerReleasedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinimizeBtn_MouseEnter(object sender, PointerEventArgs e)
+        {
+            ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.MinimizeActive));
+        }
+
+        private void MinimizeBtn_MouseLeave(object sender, PointerEventArgs e)
+        {
+            ((Image)sender).Source = new Bitmap(new MemoryStream(AppResources.Minimize));
+        }
+
+        private void MinimizeBtn_MouseLeftButtonUp(object sender, PointerReleasedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, PointerPressedEventArgs e)
+        {
+            this.BeginMoveDrag(e);
+        }
+
+        private void TitleIcon_MouseLeftButtonDown(object sender, PointerPressedEventArgs e)
+        {
+            this.BeginMoveDrag(e);
+        }
+        
+        private void TitleText_MouseLeftButtonDown(object sender, PointerPressedEventArgs e)
+        {
+            this.BeginMoveDrag(e);
+        }
+    }
+}
---
+++ Radio.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using ManagedBass;
+
+namespace RockRadioUA
+{
+    class Radio
+    {
+        static readonly object Lock = new object();
+        readonly Timer _timer;
+        int _req; // request number/counter
+        int _chan; // stream handle
+
+        private string Url = null;
+
+        public delegate void MessageHandler(string message);
+        public event MessageHandler OnStatusUpdate;
+        public event MessageHandler OnTitleAndArtistUpdate;
+
+
+        string _status = string.Empty;
+        public string Status
+        {
+            get { return _status; }
+            private set
+            {
+                _status = value;
+                OnStatusUpdate(_status);
+            }
+        }
+
+        string _titleAndArtist = string.Empty;
+        public string TitleAndArtist
+        {
+            get { return _titleAndArtist; }
+            private set
+            {
+                _titleAndArtist = value;
+                OnTitleAndArtistUpdate(_titleAndArtist);
+            }
+        }
+
+        ~Radio() => Bass.Free();
+
+        public Radio(string url)
+        {
+            if (!Bass.Init())
+                throw new Exception("Критична помилка: не вдалося ініціалізувати відтворення");
+
+            Bass.NetPlaylist = 1; // enable playlist processing
+            Bass.NetPreBuffer = 0;  // minimize automatic pre-buffering, so we can do it (and display it) instead
+
+            _timer = new Timer(50);
+            _timer.Elapsed += _timer_Tick;
+
+            Url = url;
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+
+            Bass.StreamFree(_chan);
+        }
+
+        public void Start()
+        {
+            if (Url == null)
+                return;
+
+            Task.Factory.StartNew(() =>
+            {
+                int r;
+
+                lock (Lock) // make sure only 1 thread at a time can do the following
+                    r = ++_req; // increment the request counter for this request
+
+                _timer.Stop(); // stop prebuffer monitoring
+
+                Bass.StreamFree(_chan); // close old stream
+
+                Status = "Підключення…";
+
+                var c = Bass.CreateStream(Url, 0, BassFlags.StreamDownloadBlocks | BassFlags.StreamStatus | BassFlags.AutoFree, StatusProc, new IntPtr(r));
+
+                lock (Lock)
+                {
+                    if (r != _req)
+                    {
+                        if (c != 0) // there is a newer request, discard this stream
+                            Bass.StreamFree(c);
+
+                        return;
+                    }
+
+                    _chan = c; // this is now the current stream
+                }
+
+                if (_chan == 0)
+                    Status = "Неможливо відтворити аудіопотік"; // failed to open
+                else
+                    _timer.Start(); // start prebuffer monitoring
+            });
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            // percentage of buffer filled
+            var progress = Bass.StreamGetFilePosition(_chan, FileStreamPosition.Buffer) * 100 / Bass.StreamGetFilePosition(_chan, FileStreamPosition.End);
+
+            if (progress > 75 || Bass.StreamGetFilePosition(_chan, FileStreamPosition.Connected) == 0)
+            {
+                // over 75% full (or end of download)
+                _timer.Stop(); // finished prebuffering, stop monitoring
+
+                Status = "Відтворення";
+
+                DoMeta();  // get the stream title and set sync for subsequent titles
+
+                Bass.ChannelSetSync(_chan, SyncFlags.MetadataReceived, 0, MetaSync); // Shoutcast
+                Bass.ChannelSetSync(_chan, SyncFlags.OggChange, 0, MetaSync); // Icecast/OGG            
+                Bass.ChannelSetSync(_chan, SyncFlags.End, 0, EndSync); // set sync for end of stream
+
+                Bass.ChannelPlay(_chan); // play it!
+            }
+            else
+                Status = $"Буферизація… {progress}%";
+        }
+
+        void StatusProc(IntPtr buffer, int length, IntPtr user)
+        {
+            if (buffer != IntPtr.Zero && length == 0 && user.ToInt32() == _req) // got HTTP/ICY tags, and this is still the current request
+                Status = Marshal.PtrToStringUTF8(buffer); // display status
+        }
+
+        void EndSync(int Handle, int Channel, int Data, IntPtr User) => Status = "Кінець аудіопотоку";
+
+        void MetaSync(int Handle, int Channel, int Data, IntPtr User) => DoMeta();
+
+        void DoMeta()
+        {
+            var meta = Bass.ChannelGetTags(_chan, TagType.META);
+
+            if (meta != IntPtr.Zero)
+            {
+                var data = Marshal.PtrToStringUTF8(meta);  // got Shoutcast metadata
+
+                var i = data.IndexOf("StreamTitle='"); // locate the title
+
+                if (i == -1)
+                {
+                    TitleAndArtist = string.Empty;
+
+                    return;
+                }
+
+                var j = data.IndexOf("';", i); // locate the end of it
+
+                if (j != -1)
+                    TitleAndArtist = data.Substring(i + 13, j - i - 13);
+                else
+                    TitleAndArtist = string.Empty;
+            }
+        }
+    }
+}
